1cabb074798b8f7a60719dc4b4c95c437282ef3d
岸雄斗 <k.yuto1119@icloud.com>
2020-05-29 15:45:40 +0000

    laravel18 課題の修正　index

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,9 +9,8 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                
+            
                                     @endif
                                 </div>
                                 <div class="title p-2">
@@ -44,8 +43,6 @@
                                 </div>
                             </div>
                             <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
                                 @endif
                             </div>
                         </div>
